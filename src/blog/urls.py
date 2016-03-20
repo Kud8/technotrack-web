6c63b8a6e9c13ea3a1d8027d96b9ab59472b9a9a
dmitry <mitya-kudr@mail.ru>
2016-03-20 17:57:27 +0300

    First task

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,5 +5,5 @@ from .views import *
 
 urlpatterns = [
     url(r'^posts/$', PostList.as_view(), name="post_list"),
-    url(r'^(?P<post_id>\d+)/$', show_post, name="post_id"),
+    url(r'^(?P<pk>\d+)/$', PostView.as_view(), name="post_detail"),
 ]
