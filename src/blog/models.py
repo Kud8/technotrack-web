2753b8b153f57cd0eb9501dfea355163788b26da
kud8 <mitya-kudr@mail.ru>
2016-03-20 21:25:27 +0300

    First task completed

--- src/blog/models.py
+++ src/blog/models.py
@@ -6,6 +6,5 @@ class Post(models.Model):
     title = models.CharField(max_length=140)
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-#    post_id = 
 
 # Create your models here.
--- src/blog/views.py
+++ src/blog/views.py
@@ -12,7 +12,4 @@ class PostView(DetailView):
     template_name = 'blog/post.html'
     context_object_name = 'post'
     
-def show_post(request, post_id=0):
-    return render(request, 'post.html', {"post_id": post_id})
-    
 # Create your views here.
