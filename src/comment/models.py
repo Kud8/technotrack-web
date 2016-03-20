caad06157de782bd49ccd1d846a3e829580279b7
dmitry <mitya-kudr@mail.ru>
2016-03-20 20:18:39 +0300

    Comments

--- src/comment/models.py
+++ src/comment/models.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime
 
 class Comment(models.Model):
     text = models.TextField()
     post = models.ForeignKey('blog.Post')
+    pub_date = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
